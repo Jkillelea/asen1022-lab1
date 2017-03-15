af7aa1044c5f12a97f75d9a44b6775bd0e33fe2c
Jacob Killelea <Jkillelea@protonmail.ch>
2017-03-15 08:07:11 -0600

    deleted

--- zero.m
+++
@@ -1,8 +0,0 @@
-function data_out = zero(data)
-  % Move the origin point to zero
-  data.load = data.load - data.load(1); % first one at zero
-  data.extensometer_position = data.extensometer_position - data.extensometer_position(1); % first one at zero
-
-  data_out = data;
-  return
-end
