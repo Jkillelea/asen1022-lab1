c4df8bace7d9532f5f084bd83014babfe1020af1
Jacob Killelea <Jkillelea@protonmail.ch>
2017-03-15 08:30:25 -0600

    make sure to add escape.m to the list of files

--- escape.m
+++ escape.m
@@ -1,5 +1,18 @@
 function out = escape(in_str)
-% prepends any char from the escape_symbols string with a '\' so it displays properly in a plot title
+  % ASEN 1022 - Spring 2017
+  % This code is for the first and only lab in this class
+  % Group 6: Monday 3PM-4PM
+  % ===================================
+  % Firth, Samuel (Group Leader)
+  % Stetz, Hugo
+  % Haugland, Amelia
+  % Killelea, Jacob
+  % Hanson, Sean
+
+  % There are two files in this program:
+  % main.m      -> handles analyzing all the data
+  % load_file.m -> loads data from a specified filename
+  % escape.m    -> used to ensure proper formatting in plot titles% prepends any char from the escape_symbols string with a '\' so it displays properly in a plot title
   escape_symbols = '_';
 
   out = ''; % output string
